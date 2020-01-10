a9bd6dd05fde75009a13410074f07d555313524c
Shiv Sharma <shivsharmavictory@gmail.com>
2020-01-10 14:16:33 +0530

    remove testapp from the settings.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp import views
 
 urlpatterns = [
+    path('hello',views.index),
     path('admin/', admin.site.urls),
 ]
